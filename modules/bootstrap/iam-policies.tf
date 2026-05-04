5cfef3eaa31ac124244865e6dada893d25bcf773
vigalcon <vgalcon92+github@gmail.com>
2026-05-04 16:32:40 +0100

    feat: add SSM and CloudWatch Logs paths for project_name/stepfunction/function_name structure

--- modules/bootstrap/iam-policies.tf
+++ modules/bootstrap/iam-policies.tf
@@ -216,8 +216,8 @@ data "aws_iam_policy_document" "terraform_core" {
     resources = [
       "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/bootstrap/*",
       "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/datalake/*",
-      "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/lambda/*",
-      "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/stepfunction/*",
+      "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/*/stepfunction/*",
+      "arn:aws:ssm:*:${local.account_id}:parameter/${var.environment}/*/lambda/*",
     ]
   }
 
